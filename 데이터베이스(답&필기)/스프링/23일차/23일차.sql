3b93187d60deb96b5ecd976fecaba5db97554203
qdw77 <qkrdmswn683@gmail.com>
2024-08-13 10:46:54 +0900

    Add files via upload

---
+++ 데이터베이스(답&필기)/스프링/23일차/23일차.sql
@@ -0,0 +1,5 @@
+select*from tb_student_info;
+
+-- http://localhost:8080/student?studentName=%EB%B0%95%EC%9D%80%EC%A3%BC 
+-- http://localhost:8080/student?studentName=%ED%99%8D%EA%B8%B8%EB%8F%99
+ 
