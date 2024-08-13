2d1089f6626f617f435b3c9dcca7731550d8d034
qdw77 <qkrdmswn683@gmail.com>
2024-08-13 10:54:05 +0900

    Add files via upload

---
+++ 데이터베이스(답&필기)/스프링/스프링/21일차/21일차 추가.sql
@@ -0,0 +1,4 @@
+select * from tb_parking_history;
+select * from tb_parking_info;
+select parking_location as "주차 위치", parking_car_number as "차량 번호",
+(select parking_type as "" from ),parking_time as "출입 시간" from tb_parking_history;
